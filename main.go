41ba1b2ce1ab6c6e0c87b157a36f2d84cc3db8a5
quinn-getty <quinnn.gao@gmail.com>
2023-09-30 01:19:25 +0800

    feat: 添加静态文件

--- main.go
+++ main.go
@@ -1,33 +1,55 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/quinn-getty/airdrop-go/chrome"
 )
+
+//go:embed frontend/dist/*
+var FS embed.FS
 
 func main() {
 	go func() {
 		gin.SetMode(gin.ReleaseMode)
 		r := gin.Default()
 
-		r.GET("/", func(c *gin.Context) {
-			c.String(http.StatusOK, "<h1>hi</h1>")
+		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+		r.StaticFS("/static", http.FS(staticFiles))
+		r.NoRoute(func(ctx *gin.Context) {
+			path := ctx.Request.URL.Path
+			if strings.HasPrefix(path, "/static/") {
+				reader, err := staticFiles.Open("index.html")
+				if err != nil {
+					log.Fatal("err")
+				}
+				defer reader.Close()
+				stat, err := reader.Stat()
+				if err != nil {
+					log.Fatal("err")
+				}
+				ctx.DataFromReader(http.StatusOK, stat.Size(), "text/html", reader, nil)
+			} else {
+				ctx.Redirect(http.StatusFound, "/index.html")
+			}
 		})
 		r.Run(":8080")
 	}()
 
-	cmd := chrome.Open("http://127.0.0.1:8080")
-	log.Println(cmd)
+	cmd := chrome.Open("http://127.0.0.1:8080/static")
 
 	chSignal := make(chan os.Signal, 1)
+	signal.Notify(chSignal, os.Interrupt)
+
 	select {
 	case <-chSignal:
-		signal.Notify(chSignal, os.Interrupt)
 		cmd.Process.Kill()
 	}
 }
